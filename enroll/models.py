3fa24ee27fbd074c8ff3d35f67e4a3ab6dd21b73
manishasi <manisha93singh@gmail.com>
2023-02-03 11:33:37 +0530

    add

--- enroll/models.py
+++ enroll/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class user(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
+    name = models.CharField(max_length=50,unique=True)
+    email = models.EmailField(max_length=100,unique=True)
+    password = models.CharField(max_length=100,unique=True)
--- enroll/views.py
+++ enroll/views.py
@@ -8,28 +8,46 @@ def add_show(request):
     if request.method == 'POST':
         fm = StudentRegistration(request.POST)
         if fm.is_valid():
+            # print(fm)
             # fm.save()
             nm=fm.cleaned_data['name']
+            # print(nm)
             em=fm.cleaned_data['email']
+            # print(em)
             pw=fm.cleaned_data['password']
+            # print(pw)
             reg = user(name=nm, email=em, password=pw)
             reg.save()
+            # print(reg)
+            # print(fm)
             fm = StudentRegistration()
+            return HttpResponseRedirect('/')
+            
+
     else:
         fm = StudentRegistration()
     stud = user.objects.all()
+    # for i in stud:
+        # print(i.name)
     return render(request,'enroll/addandshow.html',{'form':fm,'stu':stud})
 
 #This Function Will Update/Edit
 def update_data(request,id):
     if request.method == 'POST':
         pi = user.objects.get(pk=id)
+        print(pi)
         fm = StudentRegistration(request.POST,instance=pi )
+        print(request.POST)
+        # print(fm,"result")
         if fm.is_valid():
             fm.save()
+        print(fm)
+        return HttpResponseRedirect('/')
     else:
         pi = user.objects.get(pk=id)
+        print(pi)
         fm = StudentRegistration(instance=pi)
+        print(fm)
     return render(request,'enroll/update.html',{'form':fm})
 
 # This Function Will Delete
